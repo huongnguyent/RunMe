87ac521aa30c2bebefa90acbdd8827cd7b76279c
Huong, nguyen <thi.huong.nguyen@draphony.com>
2020-11-23 10:51:24 +0700

    .

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Huong Nguyen</Text>
+      <Text>Hello Huong Nguyen xin chao cac ban!</Text>
       <StatusBar style="auto" />
     </View>
   );
